abfe14f50bd05870a506d8b2a6791268ffc79ff5
HamzaLahbal <hamzalahbal33@gmail.com>
2025-03-19 12:36:51 +0000

    add authentification with  clerk

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Header />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -31,17 +31,13 @@ const Header = () => {
         </ul>
       </nav>
 
-      {/* buttons */}
+      {/* Login and Register buttons */}
       <div className="w-3/12 flex justify-end">
-        <a href="">
-          <svg className="h-8 p-1 hover:text-green-500 duration-200" aria-hidden="true" focusable="false" data-prefix="far" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="currentColor" d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.8-4.7 4.8-12.3 0-17zm-300.5-99.5c-77.7 0-141.5-63.8-141.5-141.5S130.3 86.5 208 86.5c77.7 0 141.5 63.8 141.5 141.5S286.2 369.4 208 369.4z"></path>
-          </svg>
+        <a href="/login" className="p-4 text-lg font-semibold text-green-500 hover:text-green-600 duration-200 cursor-pointer">
+          Login
         </a>
-        <a href="">
-          <svg className="h-8 p-1 hover:text-green-500 duration-200" aria-hidden="true" focusable="false" data-prefix="far" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="currentColor" d="M256 0c70.7 0 128 57.3 128 128 0 70.7-57.3 128-128 128S128 198.7 128 128C128 57.3 185.3 0 256 0zM256 192c53 0 96-43 96-96 0-53-43-96-96-96S160 43 160 96C160 149 203 192 256 192zM256 224c-70.7 0-128 57.3-128 128 0 35.3 28.7 64 64 64h128c35.3 0 64-28.7 64-64C384 281.3 326.7 224 256 224z"></path>
-          </svg>
+        <a href="/register" className="ml-4 p-4 text-lg font-semibold text-green-500 hover:text-green-600 duration-200 cursor-pointer">
+          Register
         </a>
       </div>
     </header>
