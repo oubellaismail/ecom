d8eaf05118e1bd25a3d97951dd6611533ee1ce56
Abdelhakkadir <127686769+Abdelhakkadir@users.noreply.github.com>
2025-04-01 15:20:40 +0000

    Add components to App.js

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import HomeCarousel from './components/HomeCarousel';
+import FeaturedProducts from './components/FeaturedProducts';
+import NewArrivals from './components/NewArrivals';
+import Footer from './components/Footer';
+import Profile from './components/Profile';
+
+function HomePage() {
+  return (
+    <>
+      <HomeCarousel />
+      <FeaturedProducts />
+      <NewArrivals />
+    </>
+  );
+}
 
 function App() {
   return (
@@ -11,8 +26,10 @@ function App() {
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<h1 className="text-center mt-5">Bienvenue sur mon site</h1>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
