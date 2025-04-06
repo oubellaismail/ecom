975d6f21530ab9451e3d67fe376b03b508f2de13
Abdelhakkadir <127686769+Abdelhakkadir@users.noreply.github.com>
2025-04-06 11:39:16 +0000

    add global auth context into index.js

--- front/src/index.js
+++ front/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { AuthProvider } from './context/AuthContext';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
