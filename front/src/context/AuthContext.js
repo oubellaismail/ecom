88895af0be70c344ac1035d206b12e3f76a4819a
Abdelhakkadir <127686769+Abdelhakkadir@users.noreply.github.com>
2025-04-06 11:33:03 +0000

    creat a global auth context to know if the user is logged or not

---
+++ front/src/context/AuthContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('authToken'));
+
+  const login = (token) => {
+    localStorage.setItem('authToken', token);
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('authToken');
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    setIsLoggedIn(!!localStorage.getItem('authToken'));
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
